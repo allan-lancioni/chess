b6fa5b8583b76614c2ef5d24a6f980d874956879
Allan Lancioni <allanlancioni@allanlancioni.com>
2024-11-08 23:06:18 -0300

    feat: include tailwind and basic style structure

---
+++ src/Board.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Board: React.FC = () => {
+  return (
+    <div>
+      <h1>Chess Board</h1>
+      {/* Add your board rendering logic here */}
+    </div>
+  );
+};
+
+export default Board;
