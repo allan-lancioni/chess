cbcb54c3f94b326f13e6dce95b8366003ef814ad
Allan Lancioni <allanlancioni@allanlancioni.com>
2024-11-09 00:37:24 -0300

    feat(Board): responsive stylized and functional board

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,55 @@
+import { useRef } from "react";
 import Board from "./Board";
 
 function App() {
+  const boardContainer = useRef<HTMLDivElement>(null);
+
   return (
-    <main className="grid grid-cols-6 gap-4 h-screen">
-      <aside className="col-span-1 bg-base-950 text-white p-4">
-        <h2 className="text-lg font-semibold mb-4">Sidebar</h2>
-        <ul className="space-y-2">
-          <li>
-            <a href="#" className="block hover:bg-gray-700 p-2 rounded">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block hover:bg-gray-700 p-2 rounded">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block hover:bg-gray-700 p-2 rounded">
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block hover:bg-gray-700 p-2 rounded">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </aside>
-      <div className="col-span-3 bg-gray-400">
-        <Board />       
+    <main className="grid md:grid-cols-6 gap-4 h-screen">
+      <Sidebar />
+      <div className="md:col-span-3 bg-gray-400 flex items-center justify-center" ref={boardContainer}> 
+        <Board boardContainerRef={boardContainer} />
       </div>
-      <div className="col-span-2 bg-gray-600 p-4">
-        <div className="rounded bg-base-800 h-full">
-          
-        </div>
-      </div>
+      <RightColumn />
     </main>
+  );
+}
+
+function Sidebar() {
+  return (
+    <aside className="hidden md:block col-span-1 bg-base-950 text-white p-4">
+      <h2 className="text-lg font-semibold mb-4">Sidebar</h2>
+      <ul className="space-y-2">
+        <li>
+          <a href="#" className="block hover:bg-gray-700 p-2 rounded">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="block hover:bg-gray-700 p-2 rounded">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" className="block hover:bg-gray-700 p-2 rounded">
+            Services
+          </a>
+        </li>
+        <li>
+          <a href="#" className="block hover:bg-gray-700 p-2 rounded">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+function RightColumn() {
+  return (
+    <div className="hidden md:block col-span-2 bg-gray-600 p-4">
+      <div className="rounded bg-base-800 h-full"></div>
+    </div>
   );
 }
 
--- src/Board.tsx
+++ src/Board.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import { useEffect, useState } from "react"
 
-const Board: React.FC = () => {
+type BoardProps = { boardContainerRef: React.RefObject<HTMLDivElement> }
+type SquareSize = { width: number, height: number }
+
+const baseArray = Array(8).fill(0).map((_, i) => i + 1);
+const rows: number[] = Array.from(baseArray).reverse();
+const columns: string[] = Array.from(baseArray).map(row => String.fromCharCode(96 + row))
+
+const getSquareSize = (value: number): SquareSize => ({ width: value, height: value })
+
+function Board({ boardContainerRef }: BoardProps) {
+
+  const [boardSize, setBoardSize] = useState(0)
+  const [squareSize, setSquareSize] = useState(0)
+  
+  useEffect(() => {
+    const handleResize = () => {
+      const { width, height } = boardContainerRef.current?.getBoundingClientRect() || { width: 0, height: 0 }
+      const _boardSize = Math.min(width, height)
+      setBoardSize(_boardSize)
+      setSquareSize(_boardSize / 8)
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)    
+    return () => window.removeEventListener('resize', handleResize)
+  }, [boardContainerRef])
+
   return (
-    <div>
-      <h1>Chess Board</h1>
-      {/* Add your board rendering logic here */}
-    </div>
+    <section 
+      style={getSquareSize(boardSize)}
+      className="grid grid-cols-8 gap-0 rounded-sm overflow-hidden">
+      {
+        rows.map((row, i) => (
+          columns.map((col, j) => {
+            const color = (i + j) % 2 === 0 ? 'bg-base-500' : 'bg-base-200'
+            return (
+              <div 
+                key={`square_${col+row}`}
+                style={getSquareSize(squareSize)}
+                className={`h-14 w-14 flex justify-center items-center ${color}`}>
+              <p className="text-base-950">{col+row}</p>
+            </div>
+            )
+          })
+        ))
+      }
+    </section>
   );
 };
 
